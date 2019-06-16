7a682779336e2e886933cf7a17a63a57853ed99c
cheng ma <macheng2025@gmail.com>
2019-06-16 17:47:12 +0800

    feat: add  bootstrap  and  renderer structure

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const { ipcRenderer, remote } = require('electron')
+const { BrowserWindow } = remote
+const win = new BrowserWindow()
+
+const dom = win.loadFile('index.html')
+const fs = require('fs')
+
+const root = fs.readdirSync('/')
+
+document.documentElement.innerHTML = `${root}`
+ipcRenderer.send('message', root)
